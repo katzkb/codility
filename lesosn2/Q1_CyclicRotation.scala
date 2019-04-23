04352a857a503ff2664d1f1323d3d2779598af23
katzkb <katsuya7kubo@gmail.com>
2019-04-23 17:45:30 +0900

    Add lesson2 Q1

---
+++ lesosn2/Q1_CyclicRotation.scala
@@ -0,0 +1,7 @@
+// https://app.codility.com/demo/results/trainingK2WDVW-N9Y/
+object Solution {
+  def solution(a: Array[Int], k: Int): Array[Int] = {
+    // write your code in Scala 2.12
+    if (k == 0 || a.isEmpty) a else solution(a.last +: a.init, k - 1)
+  }
+}
