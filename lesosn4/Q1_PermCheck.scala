9e015b3c0e473ee398ea2e511f92096e1b3d4686
katzkb <katsuya7kubo@gmail.com>
2019-04-25 13:36:22 +0900

    Add Lesson4 Q1

---
+++ lesosn4/Q1_PermCheck.scala
@@ -0,0 +1,9 @@
+// https://app.codility.com/demo/results/trainingKJFU9M-YVC/
+object Solution {
+  def solution(a: Array[Int]): Int = {
+    // write your code in Scala 2.12
+    val n = a.max
+    val original = BigDecimal(0.5) * n * (n+1)
+    if (a.distinct.length == a.length && original.toInt == a.sum && a.length == n) 1 else 0
+  }
+}
