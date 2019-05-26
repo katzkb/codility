7b6ec79369456e048e9ae3007caccbef509ef9bb
7KB <katsuya7kubo@gmail.com>
2019-05-26 10:57:11 +0900

    Rebuild

---
+++ src/main/scala/lesson6/Q1_Distinct.scala
@@ -0,0 +1,7 @@
+// https://app.codility.com/demo/results/trainingD5ZQHT-6TY/
+object Solution {
+  def solution(a: Array[Int]): Int = {
+    // write your code in Scala 2.12
+    a.distinct.length
+  }
+}
