0a5dd36c8f27b209a3f8db13eacdda713b217f75
7KB <katsuya7kubo@gmail.com>
2019-05-26 11:13:51 +0900

    Add lesson6 Q2

---
+++ src/main/scala/lesson6/Q2_MaxProductOfThree.scala
@@ -0,0 +1,12 @@
+// https://app.codility.com/demo/results/trainingJM6N8E-T4T/
+object Solution {
+  def solution(a: Array[Int]): Int =  {
+    // write your code in Scala 2.12
+    val sortedA = a.sorted
+    val sizeA = a.length
+    math.max(
+      sortedA.last * sortedA(sizeA - 2) * sortedA(sizeA - 3),
+      sortedA.head * sortedA(1) * sortedA.last
+    )
+  }
+}
